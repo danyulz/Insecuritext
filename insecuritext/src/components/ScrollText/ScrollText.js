dcd49698f69b6546615ef2b5a37b4328edeeeae9
unknown <d@nielhe.com>
2021-12-01 16:57:56 -0800

    scrollText

---
+++ insecuritext/src/components/ScrollText/ScrollText.js
@@ -0,0 +1,14 @@
+import './scrollText.css'
+
+export default function ScrollText() {
+    return (
+        <div className="scrollText">
+            <p class="marquee">
+                <span>This is text - This is text - This is text - This is text - This is text - This is text - This is text - This is text - This is text - This is text - This is text - This is text -&nbsp;</span>
+            </p>
+            <p class="marquee marquee2">
+                <span>This is text - This is text - This is text - This is text - This is text - This is text - This is text - This is text - This is text - This is text - This is text - This is text -&nbsp;</span>
+            </p>
+        </div>
+    )
+}
